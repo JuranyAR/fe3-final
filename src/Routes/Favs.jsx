8ebb938f392de7b25fccd455442617e3801dfeed
Jurany Ramirez <juranyr9@gmail.com>
2024-09-14 00:35:36 -0500

    add styles button

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -8,7 +8,7 @@ const Favs = () => {
   const { state } = useContext(ContextGlobal);
 
   return (
-    <div className={state.theme === 'dark' ? 'dark' : 'light'}>
+    <div className={state.theme === 'dark' ? 'favs dark' : 'favs light'}>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
         {/* este componente debe consumir los destacados del localStorage */}
